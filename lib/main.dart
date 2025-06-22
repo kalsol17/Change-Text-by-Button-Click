24f1ce41bbbf795a3598b11125f9d6e702f2e94e
kaleab solomon <kaleab24gebreselassie@gmail.com>
2025-06-22 14:16:55 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int counter=0;
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
